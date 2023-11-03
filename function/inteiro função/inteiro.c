18279a021a6a027aab54f4096c803800df77af9d
AYAM GIL <ayamgilmarto16@gmail.com>
2023-11-03 14:41:39 -0300

    add comentario

--- function/inteiro função/inteiro.c
+++ function/inteiro função/inteiro.c
@@ -16,16 +16,20 @@ void verificador(int n1)
         res = 0;
     }
     printf("\no resultado e: %d", res);
-}// verifica se o numero e inteiro
+} // verifica se o numero e inteiro
 int main()
 {
     int n1;
 
     printf("\nqual o valor do inteiro?: ");
-    scanf("%d", &n1);// recebe o valor 
+    scanf("%d", &n1); // recebe o valor
 
     printf("\nse o numero for positivo aparecera 1");
     printf("\nse o numero for negatico aparecera -1");
     printf("\nse o numero for zero aparecera 0\n");
-    verificador(n1);// retorna o valor 
+    verificador(n1); // retorna o valor
 }
+
+// Faça uma função que recebe um valor inteiro e verifica se o valor é positivo,
+// negativo ou zero. A função deve retornar 1 para valores positivos, -1 para negativos e
+// 0 para o valor 0.
