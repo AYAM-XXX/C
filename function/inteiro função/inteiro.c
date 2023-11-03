d528716f3eb0b6498c65beb6f3429b993d4c689a
AYAM GIL <ayamgilmarto16@gmail.com>
2023-11-03 14:39:55 -0300

    inteiro função

---
+++ function/inteiro função/inteiro.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void verificador(int n1)
+{
+    int res;
+    if (n1 < 0)
+    {
+        res = -1;
+    }
+    else if (n1 > 0)
+    {
+        res = 1;
+    }
+    else
+    {
+        res = 0;
+    }
+    printf("\no resultado e: %d", res);
+}// verifica se o numero e inteiro
+int main()
+{
+    int n1;
+
+    printf("\nqual o valor do inteiro?: ");
+    scanf("%d", &n1);// recebe o valor 
+
+    printf("\nse o numero for positivo aparecera 1");
+    printf("\nse o numero for negatico aparecera -1");
+    printf("\nse o numero for zero aparecera 0\n");
+    verificador(n1);// retorna o valor 
+}
