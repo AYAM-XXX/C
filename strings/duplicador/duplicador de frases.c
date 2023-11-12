d981d80ade6a6afc1efed06882a4f6fb6600d6e1
AYAM GIL <ayamgilmarto16@gmail.com>
2023-11-12 15:12:12 -0300

    duplicador

---
+++ strings/duplicador/duplicador de frases.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char frase[70];
+    int i, j = 0;
+ for (i = 0; i < 70; i++){
+        frase[i] = 0;
+    }
+    printf("digite a frase: ");
+    gets(frase);
+   
+    for (i = 0; i < 70; i++)
+    {
+        printf("%c",frase[i]);
+        if (frase[i] == ' ' || frase[i] == '.')
+        {
+            for (j = j; j < i; j++)
+            {
+                while(frase[j] == ' '){
+                j++;
+                }    
+                printf("%c",frase[j]);    
+                 
+                   
+              
+              
+            }
+        }
+    }
+    
+    
+}
