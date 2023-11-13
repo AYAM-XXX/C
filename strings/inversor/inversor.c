adcf5f2a64162455b7feeeea3e13b169abed0107
AYAM GIL <ayamgilmarto16@gmail.com>
2023-11-12 22:54:15 -0300

    inversor

--- strings/inversor/inversor.c
+++ strings/inversor/inversor.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main()
+{
     int i;
     char frase[40];
- for(i = 0; i < 40 ;i++){
-       frase[i] = 0;
+    for (i = 0; i < 40; i++)
+    {
+        frase[i] = 0;
     }
     printf("qual a frase: ");
     gets(frase);
-    for(i = 39; i >= 0 ;i--){
-        printf("%c",frase[i]);
+    for (i = 39; i >= 0; i--)
+    {
+        printf("%c", frase[i]);
     }
 }
