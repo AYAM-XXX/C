b37cd4c74a2ee0df40fa89f91f8d6aec1e4df1b8
AYAM GIL <ayamgilmarto16@gmail.com>
2023-11-19 09:21:57 -0300

    arrumando categoria nadador

--- function/categoria nadador/nadador.c
+++ function/categoria nadador/nadador.c
@@ -2,47 +2,41 @@
 
 int classificador(int idade)
 {
-    int classe;
     if (idade >= 5 && idade <= 7)
     {
-        classe = printf("\nInfantil A");
-    }
-    else
-    {
+        printf("\nInfantil A");
     } // retorna Infantil A
-
-    if (idade >= 8 && idade <= 10)
-    {
-        classe = printf("\nInfantil B");
-    }
     else
     {
-    } // retorna Infantil B
-
-    if (idade >= 11 && idade <= 13)
-    {
-        classe = printf("\njuvenil A");
+        if (idade >= 8 && idade <= 10)
+        {
+            printf("\nInfantil B");
+        } // retorna Infantil B
+        else
+        {
+            if (idade >= 11 && idade <= 13)
+            {
+                printf("\njuvenil A");
+            } // retorna juvenil A
+            else
+            {
+                if (idade >= 14 && idade <= 17)
+                {
+                    printf("\njuvenil B");
+                } // retorna juvenil B
+                else
+                {
+                    if (idade >= 18)
+                    {
+                        printf("\nadulto");
+                    } // retorna adulto
+                    else
+                    {
+                    }
+                }
+            }
+        }
     }
-    else
-    {
-    } // retorna juvenil A
-
-    if (idade >= 14 && idade <= 17)
-    {
-        classe = printf("\njuvenil B");
-    } // retorna juvenil B
-    else
-    {
-    }
-    if (idade >= 18)
-    {
-        classe = printf("\nadulto");
-    } // retorna adulto
-    else
-    {
-    }
-
-    return classe;
 }
 int main()
 {
@@ -59,8 +53,8 @@ int main()
     classificador(idade); // retorna a classe
 }
 
-// Faça um programa que implemente uma função que recebe a idade de um nadador por 
-//parâmetro e retorna, também por parâmetro, a categoria desse nadador de acordo com a
+// Faça um programa que implemente uma função que recebe a idade de um nadador por
+// parâmetro e retorna, também por parâmetro, a categoria desse nadador de acordo com a
 // tabela abaixo:
 
 //    Idade      Categoria
