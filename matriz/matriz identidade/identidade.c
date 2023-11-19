c6cb0e5a174f567ded87afe7f700cb9cc8bd5cb9
AYAM GIL <ayamgilmarto16@gmail.com>
2023-11-19 18:49:22 -0300

    melhoramento de code

--- matriz/matriz identidade/identidade.c
+++ matriz/matriz identidade/identidade.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 int main()
 {
-    int matriz[4][4];
-    int i,j;
+  int tam = 4, soma = 0;
+  int matriz[tam][tam];
+  int i, j;
 
-    for (i = 0; i < 4; i++)
+  for (i = 0; i < tam; i++)
+  {
+    for (j = 0; j < tam; j++)
     {
-        for ( j = 0; j < 4; j++)
-        {
-            printf("\ndigite o valor [%d][%d]: ",i,j);
-            scanf("%d", &matriz[i][j]);
-        }
+      printf("\ndigite o valor [%d][%d]: ", i, j);
+      scanf("%d", &matriz[i][j]);
     }
-    for ( i = 0; i < 4; i++)
+  }
+  for (i = 0; i < tam; i++)
+  {
+    for (j = 0; j < tam; j++)
     {
-        for ( j = 0; j < 4; j++)
-        {
-            if (i == 3 && j > 0)
-            {
-                printf("\no valor da posicao[%d][%d] e: %d", i, j, matriz[i][j]);
-            }
-            if (i == 2 && j > 1)
-            {
-                printf("\no valor da posicao[%d][%d] e: %d", i, j, matriz[i][j]);
-            }
-            if (i == 1 && j > 2)
-            {
-                printf("\no valor da posicao[%d][%d] e %d", i, j, matriz[i][j]);
-            }
-            else
-            {
-                matriz[i][j] = 0;
-            }
-        }
+      if (i + j > tam - 1)
+      {
+        printf("%d  ", matriz[i][j]);
+      }
+      else
+      {
+        printf("   ");
+      }
+      if (j > i)
+      {
+        soma += matriz[i][j];
+      }
     }
-}
+    printf("\n\n");
+  }
+  printf("\na soma da matriz identidade e: %d", soma);
+} //	Elabore um programa que leia dados em matriz 4X4 de inteiros e posteriormente imprima os elementos que estão armazenados abaixo da diagonal secundária e some os valores que estão acima da diagonal principal.
